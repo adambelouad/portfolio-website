cd08b9fbcb5e8946c4669d34a67148224648ee26
adambelouad <132090786+adambelouad@users.noreply.github.com>
2025-12-08 11:32:06 -0500

    added sppeed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import localFont from "next/font/local";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const geistSans = Geist({
